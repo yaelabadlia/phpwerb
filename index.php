3584f6897230e55582e2717ad7c3a77279289a96
yael <yaelabadlia4@gmail.com>
2025-06-23 11:56:50 +0200

    preparatioon de la page d'acceuill

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home page </title>
+</head>
+<body>
+    <h1>page d'accceuil1</h1>
+    <p>bienvenue dans notre application <strong>PHP</strong> </p>
+</body>
+</html>
